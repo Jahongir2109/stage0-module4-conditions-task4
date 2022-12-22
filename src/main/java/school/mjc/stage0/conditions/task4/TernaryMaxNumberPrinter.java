b2dd016a212de7811058061cac0d9b960ae15dc8
Jahongir2109 <jahongirachilov004@gmail.com>
2022-12-22 16:35:50 +0500

    ternary  fixed with some strange

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
-        if(first>second){
+        if(first > second){
             System.out.println(first>third?first:third);
         }else {
             System.out.println(second>third?second:third);
