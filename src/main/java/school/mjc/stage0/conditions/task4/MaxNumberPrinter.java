8d5fc3d6802366687f027156c8bb5d17b0f3f3a2
Jahongir2109 <jahongirachilov004@gmail.com>
2023-01-19 13:30:14 +0500

    i changed all to ternary

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,18 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
-        if(first>second){
-            if(first>third){
-                System.out.println(first);
-            }else {
-                System.out.println(third);
-            }
-        }else {
-            if(second>third){
-                System.out.println(second);
-            }else {
-                System.out.println(third);
-            }
-        }
+        int max=first>second? (first>third?first:third):(second>third?second:third);
+        System.out.println(max);
     }
 }
