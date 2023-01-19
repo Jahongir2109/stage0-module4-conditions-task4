8d372dba7ec3965be30d775ff6070a3043f339b0
Jahongir2109 <jahongirachilov004@gmail.com>
2023-01-19 13:40:16 +0500

    good experience

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
-        int max=first>(second>third?second:third)?first: (second>third?second:third);
-        System.out.println(max);
+        int d = first > (second > third ? second : third) ? first : ((second > third) ? second : third);
+        System.out.println(d);
     }
 }
