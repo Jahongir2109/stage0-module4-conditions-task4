24b18d2310adcde8c370cf4cb491a871451dca20
Jahongir2109 <jahongirachilov004@gmail.com>
2023-01-19 14:11:03 +0500

    max number fixed

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
-        int d = first > (second > third ? second : third) ? first : ((second > third) ? second : third);
-        System.out.println(d);
+        int max = first > (second > third ? second : third) ? first : ((second > third) ? second : third);
+        if (max == first) {
+            System.out.println(0);
+        }else {
+            System.out.println(max);
+        }
     }
 }
